0595b6b2e9b72a270681ef0735e82580ae76b22a
Snail Mail <20916990+callieapan@users.noreply.github.com>
2025-01-06 21:42:22 -0800

    commented out size in paginationLink component

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -56,7 +56,7 @@ const PaginationPrevious = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
+    //size="default"
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
@@ -71,7 +71,7 @@ const PaginationNext = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to next page"
-    size="default"
+    //size="default"
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
